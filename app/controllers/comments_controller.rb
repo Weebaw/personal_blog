35ca199698048f3720467002d5e842537efde923
Ben Steinbook <ben.steinbook@gmail.com>
2015-09-10 16:35:25 -0600

    added delete function for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = Article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comment.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
